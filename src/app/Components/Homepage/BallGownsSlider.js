351b8eea6b540dd4a0df39e450f387e2de6343d2
raj79000 <rajesh@cinuniverse.com>
2025-07-31 16:21:49 +0530

    new correction

--- src/app/Components/Homepage/BallGownsSlider.js
+++ src/app/Components/Homepage/BallGownsSlider.js
@@ -47,7 +47,7 @@ const BallGownsSlider = () => {
       <div className="container">
         <div className="global-heading-sec text-center">
           <h2 className="global-heading italic">Collections</h2>
-          <p className="italic">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className="italic">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         
         <h5 className="global-heading1 italic">Ball Gowns</h5>
         <Link href="#" className="view-all-btn">View All</Link>
--- src/app/Components/Homepage/BeSpokeSlider.js
+++ src/app/Components/Homepage/BeSpokeSlider.js
@@ -35,7 +35,7 @@ const BeSpokeSlider = () => {
         <div className="global-heading-sec text-center">
           <h2 className="global-heading italic">Create a Bespoke Bridal Outfit</h2>
           <p className="italic">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+             Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an
               unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         
         </div>
